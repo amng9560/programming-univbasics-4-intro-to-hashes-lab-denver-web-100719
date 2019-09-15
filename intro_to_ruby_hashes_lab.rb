4c38c6572c119d67d2d64e335879a453de75b3a7
Amy Nguyen <amy.nguyen730@gmail.com>
2019-09-15 17:34:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,10 +31,6 @@ def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
-  person = {name: "Sam", age:31}
-  if person[age]
-    person[age] += 1
-  else
-    puts "Key not found"
-  end
+  hash[key] ? hash[key] += 1 : hash[key] = 1
+  hash
 end
